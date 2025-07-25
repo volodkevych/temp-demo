97fccb2d608f20057b2d484963950098bb45400f
Oksana Volodkevych <13987374+volodkevych@users.noreply.github.com>
2025-07-25 14:18:17 +0200

    Update App.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div className="container">
-      <h1>Welcome to CodePipeline!</h1>
+      <h1>Welcome to AWS CodePipeline!</h1>
     </div>
   );
 };
